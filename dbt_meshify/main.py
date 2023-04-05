3aace5aec80b06f89415a3a01a39e18ee5406a2f
Grace Goheen <grace.goheen@dbtlabs.com>
2023-04-05 16:48:36 -0700

    navigate to a repo, run dbt compile, save the manifest

--- dbt_meshify/main.py
+++ dbt_meshify/main.py
@@ -1,5 +1,9 @@
 import json 
 import click
+import os
+import subprocess
+
+REPO_A = os.getenv("REPO_A", "test-projects/source-hack/src_proj_a")
 
 @click.group()
 def cli():
@@ -7,5 +11,12 @@ def cli():
 
 @cli.command(name="merge")
 def merge():
-    """This will do something"""
-    print("hey dave i solved it")
+    """This will navigate to repo_a, run dbt compile, load the manifest.json, and print the nodes"""
+
+    print("First navigate to " + REPO_A)
+    os.chdir(REPO_A)
+    subprocess.run(["dbt", "compile"])
+    repo_a_manifest = open(REPO_A + '/target/manifest.json')
+    manifest_a = json.load(repo_a_manifest)
+
+    print(manifest_a['nodes'])
