6286e2c01fc1ab54a94fe2f9133ca0aaf2899f8b
Dave Connors <dave.connors@fishtownanalytics.com>
2023-05-16 14:46:45 -0500

    Deployed b507c50 with MkDocs version: 1.4.3

--- dbt_meshify/cli.py
+++
@@ -1,1 +0,0 @@
-# TODO add the logic for each meshify command to independent tasks here to make cli thinner
--- test-projects/source-hack/dest_proj_b/models/downstream_model.sql
+++
@@ -1,8 +0,0 @@
-with 
-
-upstream as (
-    select * from {{ ref('src_proj_a', 'shared_model') }}
-)
-
-select * from upstream 
-where colleague = 'grace'
