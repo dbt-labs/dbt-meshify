307cf6657e6d9c26656a96704de7831dd0b85bf4
Dave Connors <dave.connors@fishtownanalytics.com>
2023-07-06 15:26:45 -0500

    update test project setup to include seeding db

--- tests/integration/test_group_command.py
+++ tests/integration/test_group_command.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pytest
 from click.testing import CliRunner
 
-from dbt_meshify.dbt import Dbt
 from dbt_meshify.dbt_projects import DbtProject
 from dbt_meshify.main import group
 from tests.dbt_project_utils import setup_test_project, teardown_test_project
@@ -11,7 +10,7 @@ from tests.dbt_project_utils import setup_test_project, teardown_test_project
 src_path_string = "test-projects/split/split_proj"
 dest_path_string = "test-projects/split/temp_proj"
 proj_path = Path(dest_path_string)
-dbt = Dbt()
+
 
 # this test should encapsulate the following:
 # 1. group is created in the project with proper yml
@@ -31,9 +30,6 @@ dbt = Dbt()
 )
 def test_group_command(select, expected_public_contracted_models):
     setup_test_project(src_path_string, dest_path_string)
-    dbt.invoke(directory=proj_path, runner_args=["deps"])
-    dbt.invoke(directory=proj_path, runner_args=["seed"])
-    dbt.invoke(directory=proj_path, runner_args=["run"])
     runner = CliRunner()
     result = runner.invoke(
         group,
