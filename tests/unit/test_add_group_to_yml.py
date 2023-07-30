da780118d15c53bed80b5f6853e6ce26d42e3d2d
Nicholas Yager <yager@nicholasyager.com>
2023-07-30 06:57:18 -0400

    test: Update unit test for Group resource creation to leverage FileEditor

--- tests/unit/test_add_group_to_yml.py
+++ tests/unit/test_add_group_to_yml.py
@@ -1,9 +1,15 @@
+import pathlib
+
 import pytest
 from dbt.contracts.graph.nodes import Group
 from dbt.contracts.graph.unparsed import Owner
 from dbt.node_types import NodeType
 
-from dbt_meshify.storage.file_content_editors import DbtMeshFileEditor
+from dbt_meshify.change import Change, EntityType, Operation
+from dbt_meshify.storage.file_content_editors import (
+    DbtMeshFileEditor,
+    ResourceFileEditor,
+)
 from tests.unit import read_yml
 
 meshify = DbtMeshFileEditor()
@@ -64,20 +70,27 @@ class TestAddGroupToYML:
             resource_type=NodeType.Group,
         )
 
-    def test_adds_groups_to_empty_file(self, new_group: Group):
-        yml_dict = meshify.add_group_to_yml(
-            group=new_group, groups_yml=read_yml(group_yml_empty_file)
+    @pytest.fixture
+    def change(self, new_group: Group) -> Change:
+        return Change(
+            operation=Operation.Add,
+            entity_type=EntityType.Group,
+            identifier=new_group.name,
+            path=pathlib.Path(new_group.path),
+            data={
+                "name": new_group.name,
+                "owner": new_group.owner.to_dict(),
+            },
         )
+
+    def test_adds_groups_to_empty_file(self, change: Change):
+        yml_dict = ResourceFileEditor.update_resource({}, change)
         assert yml_dict == read_yml(expected_group_yml_no_group)
 
-    def test_adds_groups_to_existing_list_of_groups(self, new_group: Group):
-        yml_dict = meshify.add_group_to_yml(
-            group=new_group, groups_yml=read_yml(group_yml_existing_groups)
-        )
+    def test_adds_groups_to_existing_list_of_groups(self, change: Change):
+        yml_dict = ResourceFileEditor.update_resource(read_yml(group_yml_existing_groups), change)
         assert yml_dict == read_yml(expected_group_yml_existing_groups)
 
-    def test_adds_groups_updates_predefined_group(self, new_group: Group):
-        yml_dict = meshify.add_group_to_yml(
-            group=new_group, groups_yml=read_yml(group_yml_group_predefined)
-        )
+    def test_adds_groups_updates_predefined_group(self, change: Change):
+        yml_dict = ResourceFileEditor.update_resource(read_yml(group_yml_group_predefined), change)
         assert yml_dict == read_yml(expected_group_yml_no_group)
