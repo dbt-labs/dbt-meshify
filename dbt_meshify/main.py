5af261f8a7df3f4d8cdf73ce0f3f50d262714894
dave-connors-3 <73915542+dave-connors-3@users.noreply.github.com>
2023-08-08 08:17:41 -0500

    Update dbt_meshify/main.py
    Co-authored-by: Nicholas A. Yager <yager@nicholasyager.com>

--- dbt_meshify/main.py
+++ dbt_meshify/main.py
@@ -102,12 +102,13 @@ def connect(
             logger.info(
                 f"No dependencies found between {dbt_project_combo[0].name} and {dbt_project_combo[1].name}"
             )
-        else:
-            noun = "dependency" if len(dependencies) == 1 else "dependencies"
-            logger.info(
-                f"Found {len(dependencies)} {noun} between {dbt_project_combo[0].name} and {dbt_project_combo[1].name}"
-            )
-            all_dependencies.update(dependencies)
+            continue
+       
+        noun = "dependency" if len(dependencies) == 1 else "dependencies"
+        logger.info(
+              f"Found {len(dependencies)} {noun} between {dbt_project_combo[0].name} and {dbt_project_combo[1].name}"
+        )
+        all_dependencies.update(dependencies)
     if len(all_dependencies) == 0:
         logger.info("No dependencies found between any of the projects")
         return
