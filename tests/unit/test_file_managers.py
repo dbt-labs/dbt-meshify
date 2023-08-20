291b0f4ea238b55990600603663923918d91fb29
Nicholas Yager <yager@nicholasyager.com>
2023-08-20 14:15:27 -0400

    fix: YAMLFileManager removes fields containing empty lists
    Resolves bug #129

---
+++ tests/unit/test_file_managers.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+from dbt_meshify.storage.file_manager import YAMLFileManager
+
+
+class TestYAMLFileManager:
+    def test_empty_lists_are_removed(self):
+        """Test that dictionaries containing fields with empty lists have those fields removed when written to YAML."""
+        path = Path("example.yml")
+        input = {"name": "example", "tests": [], "description": "An example model"}
+        file_manager = YAMLFileManager()
+        file_manager.write_file(path, input)
+
+        data = file_manager.read_file(path)
+        os.unlink(path)
+        assert data == {"name": "example", "description": "An example model"}
