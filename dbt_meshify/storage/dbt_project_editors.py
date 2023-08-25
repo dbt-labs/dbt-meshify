0e55c32d589def0b1d6022ed1dceb4f2de9796ce
Nicholas Yager <yager@nicholasyager.com>
2023-08-25 08:38:51 -0400

    fix: `require-dbt-versions` no longer serialized as a list of characters

--- dbt_meshify/storage/dbt_project_editors.py
+++ dbt_meshify/storage/dbt_project_editors.py
@@ -95,7 +95,11 @@ class DbtSubprojectCreator:
         # this one appears in the project yml, but i don't think it should be written
         contents.pop("query-comment")
         contents = filter_empty_dict_items(contents)
-        # project_file_path = self.target_directory / Path("dbt_project.yml")
+
+        # Serialize the `require-dbt-version` field into a string
+        if "require-dbt-version" in contents:
+            contents["require-dbt-version"] = "".join(contents["require-dbt-version"])
+
         return FileChange(
             operation=Operation.Add,
             entity_type=EntityType.Code,
